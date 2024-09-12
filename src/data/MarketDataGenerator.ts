914db8c6be8314f22d2f2e9665650325bbe71db5
heswell <steve@heswell.com>
2024-09-12 17:08:21 +0100

    Initial commit

---
+++ src/data/MarketDataGenerator.ts
@@ -0,0 +1,45 @@
+import { Table } from '@vuu-ui/vuu-data-test';
+import { VuuRange } from '@vuu-ui/vuu-protocol-types';
+import { generateMarketDepth } from './data-utils';
+
+const UPDATE_FREQUENCY = 250;
+
+interface UpdateGenerator {
+  setTable: (table: Table) => void;
+  setRange: (range: VuuRange) => void;
+}
+
+export class MarketDataGenerator implements UpdateGenerator {
+  private table: Table | undefined;
+  private range: VuuRange | undefined;
+  private updating = false;
+
+  setRange(range: VuuRange) {
+    this.range = range;
+    if (!this.updating && this.table) {
+      this.startUpdating();
+    }
+  }
+
+  setTable(table: Table) {
+    this.table = table;
+  }
+
+  private startUpdating() {
+    this.updating = true;
+    this.update();
+  }
+
+  update = () => {
+    if (this.range && this.table) {
+      const updatedMarketData = generateMarketDepth('VOD.L');
+      for (const updatedRow of updatedMarketData) {
+        this.table.updateRow(updatedRow);
+      }
+    }
+
+    if (this.updating) {
+      window.setTimeout(this.update, UPDATE_FREQUENCY);
+    }
+  };
+}
---
+++ src/layoutJSON.ts
@@ -0,0 +1,48 @@
+import { registerComponent } from '@vuu-ui/vuu-utils';
+import { MarketDepthFeature } from './components/market-depth';
+import { PricesTable } from './components/prices-table';
+import { Flexbox } from '@vuu-ui/vuu-layout';
+
+registerComponent('MarketDepthFeature', MarketDepthFeature, 'view');
+registerComponent('PricesTable', PricesTable, 'view');
+registerComponent('Flexbox', Flexbox, 'container');
+
+export const layoutJSON = {
+  type: 'Flexbox',
+  props: {
+    splitterSize: false,
+    style: {
+      flexDirection: 'column',
+    },
+  },
+  children: [
+    {
+      type: 'View',
+      props: {
+        resizeable: true,
+        style: {
+          flexBasis: 0,
+          flexGrow: 1,
+          flexShrink: 1,
+          height: 'auto',
+          width: 'auto',
+        },
+      },
+      children: [{ type: 'PricesTable' }],
+    },
+    {
+      type: 'View',
+      props: {
+        resizeable: true,
+        style: {
+          flexBasis: 0,
+          flexGrow: 1,
+          flexShrink: 1,
+          height: 'auto',
+          width: 'auto',
+        },
+      },
+      children: [{ type: 'MarketDepthFeature' }],
+    },
+  ],
+};
